6d056e34f3874d76625d3330cfbb22b9fe65b5f2
Khaleel Younis <khaleelyounis1@gmail.com>
2018-02-12 16:05:41 -0800

    added dev proxy for both client and sever side npm run. added api prefix proxy to routes in client side

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const PORT = process.env.PORT || 9000;
+const cors = require('cors');
 const app = express();
+
+//Cross Origin Request Middleware
+app.use(cors());
 
 // Home Route
 app.get('/', (req, res) => {
-    res.send('<h1>App is working!</h1>');
+    res.send('<h1>App is working! Now with Nodemon</h1>');
 });
 
 //Test route
 app.get('/test-route', (req, res) => {
     res.send('<h1>This is a test route, if you are seeing this all is well.</h1>');
+});
+
+app.get('/api/get-user', (req, res) => {
+    res.send({ username: 'Bobthegreat', email: 'bob@mail.com', name: 'Bob' });
+});
+
+app.get('/api/get-articles', (req, res) => {
+    res.send(['article-1', 'article-2', 'article-3']);
 });
 
 //Listen function
